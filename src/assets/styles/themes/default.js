b8921f968614f8b47e6113322e6056ea3782f848
Crummorn <igorcaze@gmail.com>
2023-07-29 19:20:24 -0300

    chore(theme): update colors

--- src/assets/styles/themes/default.js
+++ src/assets/styles/themes/default.js
@@ -1,3 +1,12 @@
 export default {
-  backgroundColor: '#F6F5FC',
+  colors: {
+    white: '#FFFFFF',
+    background: '#F6F5FC',
+    primary: {
+      dark: '#3346F0',
+      main: '#5061FC',
+      light: '#6674F4',
+      ligter: '#E0E3FF',
+    },
+  },
 };
