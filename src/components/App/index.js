4943be487bc6c1be25e344488ecf266e6716191d
Crummorn <igorcaze@gmail.com>
2023-07-30 17:43:30 -0300

    feat(routes): create and use

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,27 @@
 import { ThemeProvider } from 'styled-components';
 
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
+import Routes from '../../Routes';
 
 import * as S from './styles';
-import ContactsList from '../ContactsList';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
 
-      <S.Container>
-        <Header />
+        <S.Container>
+          <Header />
 
-        <ContactsList />
-      </S.Container>
-    </ThemeProvider>
+          <Routes />
+        </S.Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
