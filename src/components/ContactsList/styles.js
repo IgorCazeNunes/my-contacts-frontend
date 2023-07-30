1ce1cfa7624dceb1361d9ad37a2662ae1880e489
Crummorn <igorcaze@gmail.com>
2023-07-30 17:30:32 -0300

    feat(contactlist): create and use component

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -1,6 +1,13 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  hr {
+    border: ${({ theme }) => theme.colors.gray[200]} solid 1px
+  }
 `;
 
 export const Header = styled.header`
@@ -9,7 +16,7 @@ export const Header = styled.header`
   justify-content: space-between;
 
   strong {
-    color: #222222;
+    color: ${({ theme }) => theme.colors.gray[900]};
     font-size: 24px;
   }
 
@@ -28,5 +35,83 @@ export const Header = styled.header`
       color: ${({ theme }) => theme.colors.white};
       transition: all 0.2s ease-in;
     }
+  }
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  header button {
+    background: transparent;
+    border: none;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    span {
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
+  }
+`;
+
+export const ListBody = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  list-style: none;
+`;
+
+export const Card = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 16px;
+
+  background: ${({ theme }) => theme.colors.white};
+  border-radius: 4px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.04);
+`;
+
+export const CardInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  .contact-name {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    small {
+      padding: 4px;
+
+      color: ${({ theme }) => theme.colors.primary.main};
+      font-weight: bold;
+      text-transform: uppercase;
+
+      background: ${({ theme }) => theme.colors.primary.lighter};
+      border-radius: 4px;
+    }
+  }
+
+  span {
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.gray[200]};
+  }
+`;
+
+export const CardActions = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  button {
+    background: transparent;
+    border: none;
   }
 `;
